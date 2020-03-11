6672dd958332a8f00190581fe3e9809e2fc196c5
claudiacarvalhoc <claudia.a.carvalho.c@gmail.com>
2020-03-11 15:15:19 +0000

    submit-alter-io-challenge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,24 +2,15 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { GeneratorComponent }   from './generator/generator.component';
-// import { HeroesComponent }      from './heroes/heroes.component';
-// import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/generator', pathMatch: 'full' },
   { path: 'generator', component: GeneratorComponent },
-  // { path: 'payments', component: HeroDetailComponent },
 ];
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]
-})
+});
+
 export class AppRoutingModule {}
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
